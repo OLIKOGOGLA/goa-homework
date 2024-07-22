18cf6a2ba9fb3f5a70899bd4cd7a68c796575783
olikogogla <olikogogla@gmail.com>
2024-07-22 14:34:30 +0400

    first commit

---
+++ level 02/classwork/cvladi.py
@@ -0,0 +1,19 @@
+#1
+last_name = "gogla"
+print(last_name)
+
+#2
+age=12
+print(age)
+
+#3
+print(7.5/1.5)
+print(14*15)
+
+#4
+#One nail is missing
+print(14*5
+ 
+ #5  
+#It will take out as many times as 5 in 15
+print(15//5)
---
+++ level 04/homework/questions.py
@@ -0,0 +1,11 @@
+#1. What is the correct way to create a variable?:
+#a) 1dog_name
+#b) dog_name  "correct"
+#c) dog*name
+#d) dog name
+
+#2. What does case sensitive language mean?
+#It means that the Python language is case sensitive
+
+#3. What good practices do we know in Python?
+#Best practice is to use comments
