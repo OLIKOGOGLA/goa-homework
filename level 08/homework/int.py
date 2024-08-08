f56dbc8f9981f415cc12239e32b797d2fbe98568
olikogogla <olikogogla@gmail.com>
2024-08-08 18:06:23 +0400

    second commit

---
+++ level 08/homework/int.py
@@ -0,0 +1,39 @@
+#2
+age=77
+print(type(age))
+print(age)
+#3
+name="gabi"
+print(type(name))
+print(name)
+#4
+age=77.5
+print(type(age))
+print(age)
+#5
+age='42'
+print(type(age))
+print(int(age))
+print(type(age))
+#6
+age='3.14'
+print(type(age))
+print(float(age))
+print(type(age))
+#75
+name=str(input())
+print(type(name))
+#8
+name=int(input())
+print(type(name))
+#9
+name=float(input())
+print(type(name))
+#10
+name = int(input())
+print(2024-name)
+#11
+age=int(input())
+ded_age=int(input())
+print((ded_age + 15) / (age + 15))
+print(2.4545454546 * (age+15) )
