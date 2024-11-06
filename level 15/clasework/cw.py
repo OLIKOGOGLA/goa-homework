bfaba196936dcc55f986513bac247004f8333766
OLIKOGOGLA <olikogogla@gmail.com>
2024-11-06 22:34:32 +0400

    Add files via upload

---
+++ level 15/clasework/cw.py
@@ -0,0 +1,28 @@
+##1. შექმენით if-else, რომელიც ამოწმებს ტემპერატურის ცვლადი მეტია თუ არა 30-ზე, 
+# თუ არის, გამოიტანოს "it's hot outside", თუ არ არის "it's not too hot outside".
+
+
+temperature__=int(input("enter your temperature "))
+
+temperature = 30
+
+if temperature__ > temperature:
+    print("it's hot outside")
+
+if temperature__ < temperature:
+    print("it's not too hot outside")
+
+#2. შექმენით if-else, რომელიც ამოწმებს ასაკის ცვლადი მეტია თუ არა 19-ზე,
+#  თუ მეტია გამოიტანე "adult", თუ ნაკლებია გამოიტანე "teenager".
+
+age__=int(input("enter your temperature "))
+
+age = 19
+
+if age__ > age:
+    print("it's hot outside")
+
+if age__ < age:
+    print("it's not too hot outside")
+
+
