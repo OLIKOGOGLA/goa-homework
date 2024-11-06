189171cec88dd31cd9133eb9e790f55847c02600
OLIKOGOGLA <olikogogla@gmail.com>
2024-11-06 22:31:30 +0400

    Add files via upload

---
+++ level 13/homework/hw.py
@@ -0,0 +1,18 @@
+##1. for loop-ის მეშვეობით გამოიტანეთ რიცხვი 10, 7-ჯერ.
+
+for _ in range(7):
+    print(10)
+
+#2. for loop-ის მეშვეობით გამოიტანეთ 'hello' 8-ჯერ.
+
+for i in  range(8):
+    print("helou")
+
+#3. Input()-ის გამოყენებით მომხმარებელს შემოატანინეთ მისი სახელი, შემდეგ for loop-ით კი დაპრინტეთ ("hello" + მომხმარებლის სახელი) 5 ჯერ.
+
+name=input()
+for i in  range(5):
+    print("helou  " + name)
+
+
+
