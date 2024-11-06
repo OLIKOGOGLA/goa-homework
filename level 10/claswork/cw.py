406742d81015e702ba6b07f889359a2ccbf263f0
OLIKOGOGLA <olikogogla@gmail.com>
2024-11-06 22:27:01 +0400

    Add files via upload

---
+++ level 10/claswork/cw.py
@@ -0,0 +1,32 @@
+# 1. დაწერეთ conditional statmenet-ის 8 მაგალითი, გამოიყენეთ and და or .
+
+print(True and False)
+print(False and True )
+print(True and True)
+print(False and False )
+print(True or False )
+print(False or True)
+print(True or True )
+print(False or False )
+
+# 2. დაწერეთ comparison operator-ებზე 9 მაგალითი (>, <, >=..)
+
+print(5 < 7 )
+print(9 > 6 )
+print(9 <= 9)
+print(9 >= 7 )
+print(9 != 4)
+print(3 < 7)
+print(6 > 1 )
+print(9 >= 7 )
+print(3 != 3 )
+
+# 3. გადააქციეთ True და False ინტეჯერებად და დაპრინტეთ.
+
+print(int(True))
+print(int(False))
+
+# 4. მომხმარებელს ინფუთით შემოატანინეთ რიცხვი, თუ 5-ზე მეტია, გამოიტანოს True.
+
+age =int( input("enter a number: "))
+print(age > 5)
